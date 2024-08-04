50a71cf7dcac8face2ff45dfcbc479adc13ba724
dev-eshii <aadhyadeveshi8d@gmail.com>
2024-08-04 07:04:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Sticky Navigation Menu JS Code
+const nav = document.querySelector("nav");
+const scrollBtn = document.querySelector(".scroll-button a");
+
+window.onscroll = function() {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  } else {
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+};
+
+// Side Navigation Menu JS Code
+const body = document.querySelector("body");
+const navBar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = function() {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+};
+
+cancelBtn.onclick = function() {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+};
+
+// Side Navigation Bar Close While We Click On Navigation Links
+const navLinks = document.querySelectorAll(".menu li a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+    scrollBtn.style.pointerEvents = "auto";
+  });
+});
